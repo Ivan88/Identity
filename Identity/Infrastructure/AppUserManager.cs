4eb5471ae50b35de0d6c8a09b75713f522d023c1
Ivan Omelchenko <omelivan@mail.ru>
2018-03-06 08:29:50 +0200

    Добавьте файлы проекта.

---
+++ Identity/Infrastructure/AppUserManager.cs
@@ -0,0 +1,22 @@
+﻿using Identity.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace Identity.Infrastructure
+{
+	public class AppUserManager : UserManager<AppUser>
+	{
+		public AppUserManager(IUserStore<AppUser> store) : base(store)
+		{
+		}
+
+		public static AppUserManager Create(IdentityFactoryOptions<AppUserManager> options, IOwinContext context)
+		{
+			AppIdentityDbContext db = context.Get<AppIdentityDbContext>();
+			AppUserManager manager = new AppUserManager(new UserStore<AppUser>(db));
+			return manager;
+		}
+	}
+}
